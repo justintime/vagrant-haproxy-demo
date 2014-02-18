a497bc69cd5f24e3f48d5ee73f6da35092a306be
Justin Ellison <justin@techadvise.com>
2014-02-17 23:12:46 -0600

    Adding missing check param to servers

--- haproxy-setup.sh
+++ haproxy-setup.sh
@@ -36,8 +36,8 @@ backend webservers
     option httpchk
     option forwardfor
     option http-server-close
-    server web1 172.28.33.11:80 maxconn 32
-    server web2 172.28.33.12:80 maxconn 32
+    server web1 172.28.33.11:80 maxconn 32 check
+    server web2 172.28.33.12:80 maxconn 32 check
 
 listen admin
     bind *:8080
